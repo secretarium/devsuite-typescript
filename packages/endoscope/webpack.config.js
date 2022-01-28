3848cdc288a80ea0e1bb612797a0abd422b2c9da
Florian Guitton <florian@secretarium.com>
2022-01-28 18:02:18 +0000

    fix(endoscope): License plugin was too old

--- packages/endoscope/webpack.config.js
+++ packages/endoscope/webpack.config.js
@@ -1,29 +1,23 @@
 const path = require('path');
 const getNxReactWebpackConfig = require('@nrwl/react/plugins/webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { LicenseWebpackPlugin } = require('license-webpack-plugin');
 
 const getWebpackConfig = (config, { buildOptions, options }) => {
+
     const baseConfig = getNxReactWebpackConfig(config);
+    const baseOptions = { ...(buildOptions || {}), ...(options || {}) };
 
     baseConfig.entry.panel = [path.resolve(__dirname, 'src/panel.tsx')];
 
-    if (baseConfig.devServer) baseConfig.devServer.devMiddleware.writeToDisk = true;
+    if (baseConfig.devServer)
+        baseConfig.devServer.devMiddleware.writeToDisk = true;
 
-    // TODO - We must temporarily remove license extraction
-    baseConfig.plugins = baseConfig.plugins.filter((plugin) => !(plugin instanceof LicenseWebpackPlugin));
-
-    const licensePluginLocation = baseConfig.plugins.findIndex((plugin) => !(plugin instanceof LicenseWebpackPlugin)) || baseConfig.plugins.length;
-    baseConfig.plugins.splice(
-        licensePluginLocation,
-        0,
-        new HtmlWebpackPlugin({
-            title: 'Endoscope Panel',
-            filename: 'panel.html',
-            chunks: ['panel'],
-            template: path.resolve(buildOptions?.root || options?.root, buildOptions?.index || options?.index)
-        })
-    );
+    baseConfig.plugins.push(new HtmlWebpackPlugin({
+        title: 'Endoscope Panel',
+        filename: 'panel.html',
+        chunks: ['panel'],
+        template: path.resolve(baseOptions.root, baseOptions.index)
+    }));
 
     return baseConfig;
 };
