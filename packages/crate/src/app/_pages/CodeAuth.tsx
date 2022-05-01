23fa9ec35fd72f310e39da4de717a4d634825ced
Florian Guitton <florian@secretarium.com>
2022-05-01 21:59:14 +0100

    fix: Address CI compilation issue

--- packages/crate/src/app/_pages/CodeAuth.tsx
+++ packages/crate/src/app/_pages/CodeAuth.tsx
@@ -1,12 +1,14 @@
+import { FC } from 'react';
 import { useLocation } from 'react-router';
 import { parse as queryParser } from 'query-string';
-import QRCode from 'react-qr-code';
+import QRCode, { QRCodeProps } from 'react-qr-code';
 import { CachePolicies, useQuery } from '@secretarium/react';
 
 export const CodeAuth = () => {
 
     const { search, pathname } = useLocation();
     const { r: refererApplicationToken } = queryParser(search);
+    const QRCodeComp: FC<QRCodeProps> = QRCode as any;
 
     const [data, loading /*, error*/] = useQuery<string, string>({
         app: 'sfx',
@@ -19,7 +21,7 @@ export const CodeAuth = () => {
         return <div className='flex flex-col items-center gap-4'>
             <div className='px-2 py-1 bg-slate-300 rounded'>{pathname}{search}</div>
             <div className='p-5 bg-white'>
-                <QRCode size={120} value={`https://secretarium.io/a/${refererApplicationToken}`} />
+                <QRCodeComp size={120} value={`https://secretarium.io/a/${refererApplicationToken}`} />
             </div>
             <div className='px-2 py-1 text-xs text-gray-400'>{loading ? 'loading' : data}</div>
         </div>;
