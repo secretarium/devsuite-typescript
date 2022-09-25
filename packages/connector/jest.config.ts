f3c37495e82a2b7daef7fb1b03274d64bd8c7eef
Florian Guitton <florian@secretarium.com>
2022-09-25 21:31:33 +0100

    chore: Upgrade jest configuration for `ts-jest` options

--- packages/connector/jest.config.ts
+++ packages/connector/jest.config.ts
@@ -1,14 +1,11 @@
 export default {
     displayName: 'connector',
     preset: '../../jest.preset.js',
-    globals: {
-        'ts-jest': {
-            tsconfig: '<rootDir>/tsconfig.spec.json'
-        }
-    },
     testEnvironment: 'node',
     transform: {
-        '^.+\\.[tj]sx?$': 'ts-jest'
+        '^.+\\.[tj]sx?$': ['ts-jest', {
+            tsconfig: '<rootDir>/tsconfig.spec.json'
+        }]
     },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
     coverageDirectory: '../../coverage/packages/connector'
--- packages/hubber/jest.config.ts
+++ packages/hubber/jest.config.ts
@@ -1,14 +1,11 @@
 export default {
     displayName: 'hubber',
     preset: '../../jest.preset.js',
-    globals: {
-        'ts-jest': {
-            tsconfig: '<rootDir>/tsconfig.spec.json'
-        }
-    },
     testEnvironment: 'node',
     transform: {
-        '^.+\\.[tj]s$': 'ts-jest'
+        '^.+\\.[tj]s$': ['ts-jest', {
+            tsconfig: '<rootDir>/tsconfig.spec.json'
+        }]
     },
     moduleFileExtensions: ['ts', 'js', 'html'],
     coverageDirectory: '../../coverage/packages/hubber'
