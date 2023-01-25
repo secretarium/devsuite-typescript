cbef3b70994eefd288f9c0a2cb59ad9c67fa55a1
Florian Guitton <florian@secretarium.com>
2023-01-25 20:36:42 +0000

    chore: Precise TS ignore scope

--- libs/trustless-app/src/index.ts
+++ libs/trustless-app/src/index.ts
@@ -1,19 +1,16 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-// import 'assemblyscript/std/assembly/index';
-
-// import '../../../node_modules/assemblyscript/std/assembly';
 
 declare type i32 = number;
 // eslint-disable-next-line
 declare namespace i32 { }
 
-// @ts-ignore
+// @ts-ignore: decorator
 @external("env", "notify") export declare function notify(s: ArrayBuffer): i32;
 
-// @ts-ignore
+// @ts-ignore: decorator
 @external("env", "add_user_query") export declare function add_user_query(s: ArrayBuffer): void;
 
-// @ts-ignore
+// @ts-ignore: decorator
 @external("env", "add_user_transaction") export declare function add_user_transaction(s: ArrayBuffer): void;
 
 export declare type Query = (arg: ArrayBuffer) => void
