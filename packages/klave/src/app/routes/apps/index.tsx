0f339b3014dd562cbd65ef0df68aeacc67fac79f
Florian Guitton <florian@secretarium.com>
2023-02-08 16:36:48 +0000

    chore: Mask listing tab

--- packages/klave/src/app/routes/apps/index.tsx
+++ packages/klave/src/app/routes/apps/index.tsx
@@ -69,7 +69,7 @@ export const AppListing: FC = () => {
                 <NavLink to={'deployments'} className={({ isActive }) => `px-3 pb-1.5 border-b-2 ${isActive ? 'border-blue-500 text-blue-500 dark:text-white dark:border-white' : 'border-transparent text-gray-600 dark:text-gray-400 '}`}>Deployments</NavLink>
                 {/* <NavLink to={'environment'} className={({ isActive }) => `px-3 pb-1.5 border-b-2 ${isActive ? 'border-blue-500 text-blue-500 dark:text-white dark:border-white' : 'border-transparent text-gray-600 dark:text-gray-400 '}`}>Environment</NavLink> */}
                 <NavLink to={'domains'} className={({ isActive }) => `px-3 pb-1.5 border-b-2 ${isActive ? 'border-blue-500 text-blue-500 dark:text-white dark:border-white' : 'border-transparent text-gray-600 dark:text-gray-400 '}`}>Domains</NavLink>
-                <NavLink to={'listing'} className={({ isActive }) => `px-3 pb-1.5 border-b-2 ${isActive ? 'border-blue-500 text-blue-500 dark:text-white dark:border-white' : 'border-transparent text-gray-600 dark:text-gray-400 '}`}>Listing</NavLink>
+                {/* <NavLink to={'listing'} className={({ isActive }) => `px-3 pb-1.5 border-b-2 ${isActive ? 'border-blue-500 text-blue-500 dark:text-white dark:border-white' : 'border-transparent text-gray-600 dark:text-gray-400 '}`}>Listing</NavLink> */}
                 <NavLink to={'settings'} className={({ isActive }) => `px-3 pb-1.5 border-b-2 ${isActive ? 'border-blue-500 text-blue-500 dark:text-white dark:border-white' : 'border-transparent text-gray-600 dark:text-gray-400 '}`}>Settings</NavLink>
             </div>
         </div>
