96caef92a8bd8aec577305819443c0ecf8a7e6d5
Florian Guitton <florian@secretarium.com>
2023-04-18 10:38:03 +0100

    chore: Targeting new Secretarium cluster

--- apps/hubber/src/utils/secretarium.ts
+++ apps/hubber/src/utils/secretarium.ts
@@ -4,7 +4,7 @@ import logger from './logger';
 
 // const secCon = new SecretariumConnector({
 //     connection: {
-//         url: 'wss://ovh-fr-gra-2388-2.node.secretarium.org:6001',
+//         url: 'wss://wasm-dev.node.secretarium.org:6001',
 //         trustKey: 'e5hbjH7MymfUaq+0FfalmTaYM4XUOG/4caBIyvvAxboMxo9JwZUFDKc0nKrS+1OFtWhoD1pwmg6allICGcwDQw=='
 //     }
 // });
@@ -17,6 +17,7 @@ export const AppLedgerSource = {
     initialize: async () => {
         try {
             const key = await Key.createKey();
+            // await client.connect('wss://wasm-dev.node.secretarium.org:6001', key, 'rliD/CISqPEeYKbWYdwa+L+8oytAPvdGmbLC0KdvsH+OVMraarm1eo+q4fte0cWJ7+kmsq8wekFIJK0a83/yCg==');
             await client.connect('wss://wasm-dev.node.secretarium.org:5001', key, 'eOx//L640C1WcBKKuxL7Uy6EehF2rL0Ir+PqVeZKzomyJHbfEeceqftHQSnJAhaYCMO5Du5GqpNTaRdUJp46xA==');
             return;
         } catch (e) {
