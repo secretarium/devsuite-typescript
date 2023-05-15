351f0103dd1442fe87f3dba677186226cdea24cb
Florian Guitton <florian@secretarium.com>
2023-05-15 15:49:07 +0100

    chore(db,hubber): Capture instances of the GitHubApp installation

--- apps/hubber/src/app/probot/index.ts
+++ apps/hubber/src/app/probot/index.ts
@@ -1,16 +1,18 @@
 import { prisma } from '@klave/db';
 import type { Probot } from 'probot';
 import logger from '../../utils/logger';
-import { deployToSubstrate, updatePullRequestFromSubstrate } from '../controllers/deploymentController';
+import { deployToSubstrate } from '../controllers/deploymentController';
 
 const probotApp = (app: Probot) => {
     app.on([
         'ping',
-        'pull_request.opened',
-        'pull_request.synchronize',
-        'check_run.rerequested',
         'push',
-        'check_suite'
+        'pull_request',
+        'repository',
+        'check_run',
+        'check_suite',
+        'installation',
+        'installation_repositories'
     ], async (context) => {
 
         // TODO Revisit this to use tRCP router with express context
@@ -22,8 +24,59 @@ const probotApp = (app: Probot) => {
                 payload: context.payload as any
             }
         });
-
         logger.info(`New record of hook '${context.name}' ${hook.id}`);
+
+        if (context.name === 'installation') {
+            const { payload } = context;
+            if (payload.action === 'created') {
+                // TODO: Create a new record in the database
+                logger.info(`Registering new GithubApp installation ${payload.installation.id}`);
+                await prisma.installation.create({
+                    data: {
+                        source: 'github',
+                        remoteId: `${payload.installation.id}`,
+                        account: payload.installation.account.login,
+                        accountType: payload.installation.account.type.toLowerCase() as any,
+                        hookPayload: payload as any
+                    }
+                });
+
+                if (payload.repositories && payload.repositories.length > 0)
+                    for (const repo of payload.repositories) {
+                        await prisma.repository.create({
+                            data: {
+                                source: 'github',
+                                remoteId: `${repo.id}`,
+                                name: repo.name,
+                                owner: payload.installation.account.login,
+                                fullName: repo.full_name,
+                                private: repo.private,
+                                installationPayload: repo as any
+                            }
+                        });
+                    }
+            }
+        }
+
+        if (context.name === 'installation_repositories') {
+            const { payload } = context;
+            if (payload.action === 'added') {
+                if (payload.repositories_added && payload.repositories_added.length > 0)
+                    for (const repo of payload.repositories_added) {
+                        await prisma.repository.create({
+                            data: {
+                                source: 'github',
+                                remoteId: `${repo.id}`,
+                                name: repo.name,
+                                owner: payload.installation.account.login,
+                                fullName: repo.full_name,
+                                private: repo.private,
+                                installationPayload: repo as any
+                            }
+                        });
+                    }
+            }
+        }
 
         if (context.name === 'push')
             deployToSubstrate({
@@ -46,33 +99,6 @@ const probotApp = (app: Probot) => {
                     login: context.payload.sender.login,
                     avatarUrl: context.payload.sender.avatar_url,
                     htmlUrl: context.payload.sender.html_url
-                }
-            });
-
-        if (context.name === 'pull_request')
-            updatePullRequestFromSubstrate({
-                octokit: context.octokit,
-                class: context.name,
-                type: context.payload.action,
-                repo: {
-                    url: context.payload.repository.html_url,
-                    owner: context.payload.repository.owner.login,
-                    name: context.payload.repository.name
-                },
-                commit: {
-                    url: context.payload.pull_request.commits_url,
-                    ref: context.payload.pull_request.head.ref,
-                    before: context.payload.action === 'synchronize' ? context.payload.before : context.payload.pull_request.base.sha,
-                    after: context.payload.action === 'synchronize' ? context.payload.after : context.payload.pull_request.head.sha
-                },
-                pusher: {
-                    login: context.payload.sender.login,
-                    avatarUrl: context.payload.sender.avatar_url,
-                    htmlUrl: context.payload.sender.html_url
-                },
-                pullRequest: {
-                    url: context.payload.pull_request.html_url,
-                    number: context.payload.pull_request.number
                 }
             });
     });
