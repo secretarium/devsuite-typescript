d60fa6bc8254eaff0126917ab02b416cea5d54ea
Florian Guitton <florian@secretarium.com>
2023-05-16 15:37:38 +0100

    fix(hubber): Solve typescript not present for runtime DTS analysis

--- apps/hubber/webpack.config.js
+++ apps/hubber/webpack.config.js
@@ -4,6 +4,6 @@ const { composePlugins, withNx } = require('@nx/webpack');
 module.exports = composePlugins(withNx(), (config) => {
     // Update the webpack config as needed here.
     // e.g. `config.plugins.push(new MyPlugin())`
-    config.externals = config.externals.concat('@klave/compiler', 'sigstore')
+    config.externals = config.externals.concat('@klave/compiler', 'sigstore', 'typescript')
     return config;
 });
