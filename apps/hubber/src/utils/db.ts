1b2039edcdbe836f726dc35b97ad76b1a1b0d7ff
Florian Guitton <florian@secretarium.com>
2023-05-16 15:36:33 +0100

    chore(hubber): Provide database connection retrial on start

--- apps/hubber/src/utils/db.ts
+++ apps/hubber/src/utils/db.ts
@@ -1,20 +1,34 @@
 import { PrismaClient } from '@prisma/client';
-import i18n from '../i18n';
+// import i18n from '../i18n';
 import { logger } from '@klave/providers';
 
 export * from '@prisma/client';
 
 export const client = new PrismaClient();
+let reconnectAttempt = 0;
+let reconnectionTimeout: NodeJS.Timeout | undefined;
+
+const planReconnection = async () => {
+    return new Promise((resolve) => {
+        if (!reconnectionTimeout) {
+            reconnectionTimeout = setTimeout(() => {
+                clearTimeout(reconnectionTimeout);
+                reconnectionTimeout = undefined;
+                AppDataSource.initialize().then(resolve).catch(() => { return; });
+            }, 3000);
+        }
+    });
+};
 
 export const AppDataSource = {
     initialize: async () => {
         try {
             await client.$connect();
+            logger.info(`Connected to Mongo via Prisma ${process.env['MONGO_URL']}`);
             return;
-        } catch (e) {
-            logger.error(i18n.t('errors:DB_CONNECT_ERROR'));
-            logger.error(e);
-            throw new Error(i18n.t('errors:DB_CONNECT_ERROR') || '');
+        } catch (e: any) {
+            logger.error(`Connection ${++reconnectAttempt} to Mongo failed: ${e.message}`);
+            await planReconnection();
         }
     },
     stop: async () => {
