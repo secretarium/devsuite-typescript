7b06fc954f89f99420d1362a75a9b37a657a0b82
Florian Guitton <florian@secretarium.com>
2023-05-19 23:34:35 +0100

    chore(providers): Make the reconciler non-blocking

--- libs/providers/src/lib/githubReconciler.ts
+++ libs/providers/src/lib/githubReconciler.ts
@@ -4,80 +4,83 @@ import { logger } from './logger';
 
 export const githubOps = {
     initialize: async () => {
-        const octokit = await probot.auth();
-        const installationsData = await octokit.paginate(
-            octokit.apps.listInstallations,
-            {
-                per_page: 100
-            },
-            (response) => response.data
-        );
-
-        logger.info(`Found ${installationsData.length} Github App installations`);
-        for (const installation of installationsData) {
-            await prisma.installation.upsert({
-                where: {
-                    source_remoteId_account: {
-                        source: 'github',
-                        remoteId: `${installation.id}`,
-                        account: installation.account?.name ?? 'unknown' // TODO - is this right?
-                    }
-                },
-                create: {
-                    source: 'github',
-                    remoteId: `${installation.id}`,
-                    account: installation.account?.name ?? 'unknown', // TODO - is this right?
-                    accountType: 'unknown', // TODO - is this right?
-                    hookPayload: installation as any
-                },
-                update: {
-                    hookPayload: installation as any
-                }
-            });
-
-            const installationOctokit = await probot.auth(installation.id);
-            const reposData = await installationOctokit.paginate(
-                installationOctokit.apps.listReposAccessibleToInstallation,
+        const sync = async () => {
+            const octokit = await probot.auth();
+            const installationsData = await octokit.paginate(
+                octokit.apps.listInstallations,
                 {
                     per_page: 100
-                }
+                },
+                (response) => response.data
             );
 
-            const repos: typeof reposData['repositories'] = [];
-            if (reposData.repositories)
-                repos.push(...reposData.repositories);
-            if (Array.isArray(reposData))
-                repos.push(...reposData as any);
-
-            logger.info(`Syncing ${repos.length} repositories for installation ${installation.id}`);
-            for (const repo of repos) {
-                await prisma.repository.upsert({
+            logger.info(`Found ${installationsData.length} Github App installations`);
+            for (const installation of installationsData) {
+                await prisma.installation.upsert({
                     where: {
-                        source_remoteId_installationRemoteId: {
+                        source_remoteId_account: {
                             source: 'github',
-                            remoteId: `${repo.id}`,
-                            installationRemoteId: `${installation.id}`
+                            remoteId: `${installation.id}`,
+                            account: installation.account?.name ?? 'unknown' // TODO - is this right?
                         }
                     },
                     create: {
                         source: 'github',
-                        remoteId: `${repo.id}`,
-                        installationRemoteId: `${installation.id}`,
-                        name: repo.name,
-                        owner: repo.owner.login,
-                        fullName: repo.full_name,
-                        private: repo.private,
-                        installationPayload: repo as any
+                        remoteId: `${installation.id}`,
+                        account: installation.account?.name ?? 'unknown', // TODO - is this right?
+                        accountType: 'unknown', // TODO - is this right?
+                        hookPayload: installation as any
                     },
                     update: {
-                        name: repo.name,
-                        owner: repo.owner.login,
-                        fullName: repo.full_name,
-                        private: repo.private,
-                        installationPayload: repo as any
+                        hookPayload: installation as any
                     }
                 });
+
+                const installationOctokit = await probot.auth(installation.id);
+                const reposData = await installationOctokit.paginate(
+                    installationOctokit.apps.listReposAccessibleToInstallation,
+                    {
+                        per_page: 100
+                    }
+                );
+
+                const repos: typeof reposData['repositories'] = [];
+                if (reposData.repositories)
+                    repos.push(...reposData.repositories);
+                if (Array.isArray(reposData))
+                    repos.push(...reposData as any);
+
+                logger.info(`Syncing ${repos.length} repositories for installation ${installation.id}`);
+                for (const repo of repos) {
+                    await prisma.repository.upsert({
+                        where: {
+                            source_remoteId_installationRemoteId: {
+                                source: 'github',
+                                remoteId: `${repo.id}`,
+                                installationRemoteId: `${installation.id}`
+                            }
+                        },
+                        create: {
+                            source: 'github',
+                            remoteId: `${repo.id}`,
+                            installationRemoteId: `${installation.id}`,
+                            name: repo.name,
+                            owner: repo.owner.login,
+                            fullName: repo.full_name,
+                            private: repo.private,
+                            installationPayload: repo as any
+                        },
+                        update: {
+                            name: repo.name,
+                            owner: repo.owner.login,
+                            fullName: repo.full_name,
+                            private: repo.private,
+                            installationPayload: repo as any
+                        }
+                    });
+                }
             }
-        }
+        };
+        sync();
     }
 };
