99046c3a5140d57560bb3a0065e5b7d0e0422f05
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2023-05-25 14:33:58 +0100

    chore(klave): Use correct url for gitlab oauth

--- apps/klave/src/app/routes/deploy/index.tsx
+++ apps/klave/src/app/routes/deploy/index.tsx
@@ -29,11 +29,11 @@ export const Index: FC = () => {
     githubAuth.searchParams.append('redirect_uri', encodeURI(process.env['NX_KLAVE_AUTHSTATE_URL'] ?? `${window.location.origin}/auth`));
 
     const gitlabAuth = new URL('https://gitlab.com/oauth/authorize');
-    githubAuth.searchParams.append('client_id', 'Iv1.6ff39dee83590f91');
-    githubAuth.searchParams.append('response_type', 'code');
-    githubAuth.searchParams.append('scope', 'read:user,read:gpg_key,read:public_key,repo');
-    githubAuth.searchParams.append('state', state);
-    githubAuth.searchParams.append('redirect_uri', encodeURI(process.env['NX_KLAVE_AUTHSTATE_URL'] ?? `${window.location.origin}/auth`));
+    gitlabAuth.searchParams.append('client_id', 'Iv1.6ff39dee83590f91');
+    gitlabAuth.searchParams.append('response_type', 'code');
+    gitlabAuth.searchParams.append('scope', 'read:user,read:gpg_key,read:public_key,repo');
+    gitlabAuth.searchParams.append('state', state);
+    gitlabAuth.searchParams.append('redirect_uri', encodeURI(process.env['NX_KLAVE_AUTHSTATE_URL'] ?? `${window.location.origin}/auth`));
 
     return <>
         <div className='pb-5'>
@@ -42,7 +42,7 @@ export const Index: FC = () => {
         </div>
         <div className='relative h-[300px]'>
             <a href={githubAuth.toString()} className='a-like mb-3 rounded-full bg-black hover:bg-gray-900 text-white'><UilGithub color='white' />&nbsp;Connect to GitHub</a><br />
-            <a href={gitlabAuth.toString()} className='a-like rounded-full bg-[#db7130] hover:bg-[#bb472d] text-white hidden'><UilGitlab color='white' />&nbsp;Connect to GitHub</a>
+            <a href={gitlabAuth.toString()} className='a-like rounded-full bg-[#db7130] hover:bg-[#bb472d] text-white hidden'><UilGitlab color='white' />&nbsp;Connect to GitLab</a>
         </div>
     </>;
 };
