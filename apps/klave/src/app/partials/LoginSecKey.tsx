85aababdac352ae78cdb32403627ef4e36cb53a0
Florian Guitton <florian@secretarium.com>
2023-06-06 08:42:12 +0100

    feat(db,api,klave): Add support for Webauthn enrollement

--- apps/klave/src/app/partials/LoginSecKey.tsx
+++ apps/klave/src/app/partials/LoginSecKey.tsx
@@ -1,33 +1,70 @@
-import { FC, useState, useCallback, ChangeEvent, MouseEvent } from 'react';
-import { useWebAuthn } from 'react-hook-webauthn';
+import { FC, useState, useEffect, useCallback, ChangeEvent, MouseEvent } from 'react';
+import { platformAuthenticatorIsAvailable, browserSupportsWebAuthn, startAuthentication, startRegistration } from '@simplewebauthn/browser';
 import api from '../utils/api';
-// import { v4 as uuid } from 'uuid';
-// import { useAuth } from '../AuthProvider';
+import { UilSpinner } from '@iconscout/react-unicons';
+import { useLocalForage } from '../useLocalStorage';
 
 export const LoginSecKey: FC = () => {
 
-    const rpOptions = {
-        rpId: process.env['NX_KLAVE_AUTHSTATE_URL'] ?? 'localhost',
-        rpName: 'Klave Network'
-    };
-
+    const [credentials, setCredentials] = useLocalForage<Array<string>>('enrolledCredentials', []);
+    const [isRequestingWebauthnInput, setIsRequestingWebauthnInput] = useState(false);
+    const [isWebauthAvailable, setIsWebauthAvailable] = useState<null | boolean>();
     const [email, setEmail] = useState('');
     const [code, setCode] = useState('');
     const [error, setError] = useState<string>();
     const [screen, setScreen] = useState<'start' | 'code' | 'key'>('start');
-    const { getCredential, getAssertion } = useWebAuthn(rpOptions);
-    const { mutate: emailCodeMutate } = api.v0.auth.getEmailCode.useMutation();
-    const { mutate: challengeMutate } = api.v0.auth.getChallenge.useMutation();
+    const { mutate: emailCodeMutation, isLoading: emailCodeLoading } = api.v0.auth.getEmailCode.useMutation();
+    const { mutate: verifyEmailCodeMutation, isLoading: verifyEmailCodeLoading } = api.v0.auth.verifyEmailCode.useMutation();
+    const { mutate: validateWebauthnMutation, isLoading: validateWebauthnLoading } = api.v0.auth.validateWebauthn.useMutation();
+    const { mutate: registerWebauthnMutation, isLoading: registerWebauthnLoading } = api.v0.auth.registerWebauthn.useMutation();
     const { refetch: refetchSession } = api.v0.auth.getSession.useQuery();
+    const { refetch: refetchAuthOptions, isFetching: wauthOptionsLoading, error: webauthAuthError } = api.v0.auth.getWebauthAuthenticationOptions.useQuery({
+        email
+    }, {
+        enabled: false,
+        retry: false
+    });
+    const { refetch: refetchRegistrationOptions, isFetching: wregOptionsLoading, error: webauthRegError } = api.v0.auth.getWebauthRegistrationOptions.useQuery({
+        email
+    }, {
+        enabled: false,
+        retry: false
+    });
 
-    const getLoginCode = useCallback((e: MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-        e.stopPropagation();
-        if (email.length === 0) {
-            setError('Please enter your email address');
-            return;
+    useEffect(() => {
+        if (credentials?.length === 1)
+            setEmail(credentials[0] ?? '');
+    }, [credentials]);
+
+    // useEffect(() => {
+    //     if (credentials?.length === 1 && email === credentials[0])
+    //         startAuth();
+    // }, [credentials, email, startAuth]);
+
+    useEffect(() => {
+        if (webauthAuthError) {
+            setError(webauthAuthError.message ?? 'An error occured while trying to send your email code. Please try again later.');
         }
-        emailCodeMutate({
+        if (webauthRegError) {
+            setError(webauthRegError.message ?? 'An error occured while trying to send your email code. Please try again later.');
+        }
+    }, [webauthAuthError, webauthRegError]);
+
+    useEffect(() => {
+        if (isWebauthAvailable === undefined) {
+            setIsWebauthAvailable(null);
+            Promise.all([
+                browserSupportsWebAuthn(),
+                platformAuthenticatorIsAvailable()
+            ])
+                .then(statuses => statuses.reduce((prev, curr) => curr && prev, true))
+                .then(setIsWebauthAvailable)
+                .catch(() => setIsWebauthAvailable(false));
+        }
+    }, [isWebauthAvailable]);
+
+    const getLoginCode = useCallback(() => {
+        emailCodeMutation({
             email
         }, {
             onSettled(data, error) {
@@ -44,16 +81,16 @@ export const LoginSecKey: FC = () => {
                     setError('An error occured while trying to send your email code. Please try again later.');
             }
         });
-    }, [email, emailCodeMutate]);
+    }, [email, emailCodeMutation]);
 
-    const getLoginChallenge = useCallback((e: MouseEvent<HTMLButtonElement>) => {
+    const verifyEmailCode = useCallback((e: MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
         e.stopPropagation();
         if (code.length === 0) {
             setError('Please enter your email code');
             return;
         }
-        challengeMutate({
+        verifyEmailCodeMutation({
             email,
             code: code.replace(/\D/g, '')
         }, {
@@ -61,13 +98,12 @@ export const LoginSecKey: FC = () => {
                 if (error)
                     setError(error?.message ?? (JSON.parse(error.message) as any)[0]?.message ?? error.message ?? 'An error occured while trying to log you in. Please try again later.');
                 else if (data?.ok)
-                    // setScreen('key');
                     refetchSession();
                 else
                     setError('An error occured while trying to log you in. Please try again later.');
             }
         });
-    }, [code, challengeMutate, email]);
+    }, [code, verifyEmailCodeMutation, email, refetchSession]);
 
     const onChangeEmail = useCallback((e: ChangeEvent<HTMLInputElement>) => {
         if (error)
@@ -86,57 +122,126 @@ export const LoginSecKey: FC = () => {
         setCode('');
         setError(undefined);
         setScreen('start');
+        setIsRequestingWebauthnInput(false);
     }, [setEmail, setCode, setError]);
 
-    const onRegister = useCallback(async () => {
-        const credential = await getCredential({
-            challenge: 'stringFromServer',
-            userDisplayName: email,
-            userId: email,
-            userName: email
-        });
-        console.log(credential);
-    }, [getCredential, email]);
+    const isLoading = emailCodeLoading || verifyEmailCodeLoading || wauthOptionsLoading || wregOptionsLoading || registerWebauthnLoading || isRequestingWebauthnInput || validateWebauthnLoading;
 
+    const startAuth = useCallback(() => {
+        if (email.length === 0) {
+            setError('Please enter your email address');
+            return;
+        }
+        if (!isWebauthAvailable)
+            return getLoginCode();
 
-    const onAuth = useCallback(async () => {
-        const assertion = await getAssertion({ challenge: 'stringFromServer' });
-        console.log(assertion);
-    }, [getAssertion]);
+        if (credentials?.includes(email)) {
+            refetchAuthOptions()
+                .then((res) => res.data)
+                .then((options) => {
+                    if (options) {
+                        setIsRequestingWebauthnInput(true);
+                        return startAuthentication(options);
+                    }
+                    throw new Error('No authentication options available');
+                })
+                .then((auth) => new Promise((resolve, reject) => {
+                    validateWebauthnMutation({
+                        email,
+                        data: auth
+                    }, {
+                        onSuccess: resolve,
+                        onError: reject
+                    });
+                }))
+                .then((res: any) => {
+                    if (res?.ok) {
+                        refetchSession();
+                    }
+                })
+                .catch(() => {
+                    setIsRequestingWebauthnInput(false);
+                });
+            return;
+        } else {
+            refetchRegistrationOptions()
+                .then((res) => res.data)
+                .then((options) => {
+                    if (options) {
+                        setIsRequestingWebauthnInput(true);
+                        return startRegistration(options);
+                    }
+                    throw new Error('No registration options available');
+                })
+                .then((reg) => new Promise((resolve, reject) => {
+                    registerWebauthnMutation({
+                        email,
+                        data: reg
+                    }, {
+                        onSuccess: resolve,
+                        onError: reject
+                    });
+                }))
+                .then((res: any) => {
+                    if (res?.ok) {
+                        setCredentials([...(credentials ?? []), email]);
+                        refetchSession();
+                    }
+                })
+                .catch(() => {
+                    setIsRequestingWebauthnInput(false);
+                    setError('An error occured while trying to register your secure key. Please try again later.');
+                });
+        }
+
+    }, [credentials, email, getLoginCode, isWebauthAvailable, refetchAuthOptions, refetchRegistrationOptions, refetchSession, registerWebauthnMutation, setCredentials, validateWebauthnMutation]);
+
+    const handleLoginSubmit = useCallback((e: MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        startAuth();
+    }, [startAuth]);
+
+    const handleLoginCodeSubmit = useCallback((e: MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        getLoginCode();
+    }, [getLoginCode]);
 
     return <div className="text-center pb-12 md:pb-16">
         <br />
         <div className='pb-5' >
-            <h1 className='text-xl font-bold'>Email log in</h1>
-            <span>Connect via a magic code</span>
-            {/*
-             <h1 className='text-xl font-bold'>Security keys</h1>
-            <span>Connect via a hardware key</span>
-             */}
+            {isWebauthAvailable && screen === 'start'
+                ? <>
+                    <h1 className='text-xl font-bold'>Secure Key</h1>
+                    <span>Connect with your secure key.</span>
+                </>
+                : <>
+                    <h1 className='text-xl font-bold'>Email Code</h1>
+                    <span>Connect via an email code.</span>
+                </>
+            }
             <br />
             <br />
             <br />
-        </div>
+        </div >
         <form className='relative'>
             {screen === 'start' ? <>
                 <input key='emailField' value={email} onInput={onChangeEmail} alt='email' placeholder='Email address' type='email' className='text-center' />
                 <br />
                 <br />
-                <button onClick={getLoginCode} onSubmit={getLoginCode} type='submit'>Next</button>
+                <button disabled={isLoading} onClick={handleLoginSubmit} onSubmit={handleLoginSubmit} type='submit'>{isLoading ? <UilSpinner className='inline-block animate-spin' /> : isWebauthAvailable ? 'Log in with secure key' : 'Log in with magic code'}</button><br />
+                <button disabled={isLoading} onClick={handleLoginCodeSubmit} className='bg-transparent border-0 shadow-none font-normal text-sm text-slate-800 hover:text-blue-600'>Use magic code instead</button>
             </> : screen === 'code' ? <>
                 <input key='codeField' value={code} onInput={onChangeCode} alt='code' placeholder='Code' type='text' className='text-center' />
                 <br />
                 <br />
-                <button onClick={getLoginChallenge} onSubmit={getLoginChallenge} type='submit' className='mx-1'>Next</button>
-                <button onClick={resetLogin} type='button' className='mx-1'>Cancel</button>
-            </> : screen === 'key' ? <>
-                <button onClick={onRegister} type='button' className='mx-1'>Register</button>
-                <button onClick={onAuth} type='button' className='mx-1'>Sign In</button>
+                <button disabled={isLoading} onClick={verifyEmailCode} onSubmit={verifyEmailCode} type='submit' className='mx-1'>{isLoading ? <UilSpinner className='inline-block animate-spin' /> : 'Next'}</button>
                 <button onClick={resetLogin} type='button' className='mx-1'>Cancel</button>
             </> : null}
             {error ? <><br /><br /><div className='bg-red-200 p-2 w-full'>{error}</div></> : null}
         </form>
-    </div>;
+    </div >;
 };
 
 export default LoginSecKey;
