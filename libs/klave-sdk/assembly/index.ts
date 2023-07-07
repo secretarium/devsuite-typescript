a9217f0ed9fa903138d70b1ae4a30e20a9e033c6
Florian Guitton <florian@secretarium.com>
2023-07-07 18:41:24 +0100

    feat(sdk): Add Light GBM APIs

--- libs/klave-sdk/assembly/index.ts
+++ libs/klave-sdk/assembly/index.ts
@@ -25,6 +25,15 @@ declare function runtime_write_ledger_raw(table: ArrayBuffer, key: ArrayBuffer, 
 // @ts-ignore: decorator
 @external("env", "query_context")
 declare function runtime_query_context_raw(key: ArrayBuffer, value: ArrayBuffer, value_size: i32): i32;
+// @ts-ignore: decorator
+@external("env", "load_lightgbm_model")
+declare function runtime_load_lightgbm_model(name: ArrayBuffer, model: ArrayBuffer): i32;
+// @ts-ignore: decorator
+@external("env", "unload_lightgbm_model")
+declare function runtime_unload_lightgbm_model(name: ArrayBuffer): i32;
+// @ts-ignore: decorator
+@external("env", "infer_from_lightgbm_model")
+declare function runtime_infer_from_lightgbm_model(name: ArrayBuffer, data: ArrayBuffer, data_size: i32, result: ArrayBuffer, result_size: i32): i32;
 
 export class Router {
     static addQuery(queryFunctionName: ArrayBuffer): void {
@@ -44,16 +53,15 @@ class Table {
 
     getArrayBuffer(key: string): ArrayBuffer {
         let k = String.UTF8.encode(key, true);
-        let value = new ArrayBuffer(64);
+        let value = new ArrayBuffer(1);
         let result = runtime_read_ledger_raw(this.table, k, k.byteLength, value, value.byteLength);
         if (result < 0)
-            return new ArrayBuffer(0); // todo : report error (or not found ?)
+            return new ArrayBuffer(0); //TODO: Report error (or not found ?)
         if (result > value.byteLength) {
-            // buffer not big enough, retry with a properly sized one
             value = new ArrayBuffer(result);
             result = runtime_read_ledger_raw(this.table, k, k.byteLength, value, value.byteLength);
             if (result < 0)
-                return new ArrayBuffer(0); // todo : report errors
+                return new ArrayBuffer(0); //TODO: Report errors
         }
         return value
     }
@@ -80,16 +88,15 @@ export class Context {
 
     static getArrayBuffer(key: string): ArrayBuffer {
         let k = String.UTF8.encode(key, true);
-        let value = new ArrayBuffer(64);
+        let value = new ArrayBuffer(1);
         let result = runtime_query_context_raw(k, value, value.byteLength);
         if (result < 0)
-            return new ArrayBuffer(0); // todo : report error (or not found ?)
+            return new ArrayBuffer(0); //TODO: Report error (or not found ?)
         if (result > value.byteLength) {
-            // buffer not big enough, retry with a properly sized one
             value = new ArrayBuffer(result);
             result = runtime_query_context_raw(k, value, value.byteLength);
             if (result < 0)
-                return new ArrayBuffer(0); // todo : report errors
+                return new ArrayBuffer(0); //TOTO: Report errors
         }
         return value;
     }
@@ -117,6 +124,39 @@ export class Notifier {
     static sendJson<T = unknown>(message: T): i32 {
         let buf = String.UTF8.encode(JSON.stringify<T>(message), true);
         return runtime_notify(buf);
+    }
+}
+
+export class ML {
+
+    static loadLightGBMModel(name: string, model: string): i32 {
+        // work around json-as bug : an invalid json with a "real" line feed in a string field would be parsed,
+        // but not if the string is properly formed with the two-characters escape sequence '\' followed by 'n'
+        // as we don't want to try to pass invalid json :
+        let tmodel = model.replaceAll('\\n', '\n');
+        let nameBuf = String.UTF8.encode(name, true);
+        let modelBuf = String.UTF8.encode(tmodel, true);
+        return runtime_load_lightgbm_model(nameBuf, modelBuf);
+    }
+
+    static unloadLightGBMModel(name: string): i32 {
+        let nameBuf = String.UTF8.encode(name, true);
+        return runtime_unload_lightgbm_model(nameBuf);
+    }
+
+    static inferLightGBMModel(name: string, data: Float64Array): Float64Array {
+        let nameBuf = String.UTF8.encode(name, true);
+        let value = new Float64Array(1);
+        let result = runtime_infer_from_lightgbm_model(nameBuf, data, data.byteLength, value, value.byteLength);
+        if (result < 0)
+            return new Float64Array(0); //TODO: Report error (or not found ?)
+        if (result > value.byteLength) {
+            value = new Float64Array(result);
+            result = runtime_infer_from_lightgbm_model(nameBuf, data, data.byteLength, value, value.byteLength);
+            if (result < 0)
+                return new Float64Array(0); //TODO: Report errors
+        }
+        return value;
     }
 }
 
