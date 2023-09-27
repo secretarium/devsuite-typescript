6515eeaf91687110855a5a536f56ac6c1447c5f1
Florian Guitton <florian@secretarium.com>
2023-09-27 09:06:53 +0100

    chore!: Remove Klave packages

--- apps/hubber/src/app/middleware/i18n.ts
+++
@@ -1,4 +0,0 @@
-import i18nextExpressMiddleware from 'i18next-http-middleware';
-import i18n from '../../i18n';
-
-export const i18nextMiddleware = i18nextExpressMiddleware.handle(i18n);
--- libs/klave-api/src/router.ts
+++
@@ -1,8 +0,0 @@
-import { createTRPCRouter } from './trpc';
-import { v0Router } from './v0';
-
-export const router = createTRPCRouter({
-    v0: v0Router
-});
-
-export type Router = typeof router;
