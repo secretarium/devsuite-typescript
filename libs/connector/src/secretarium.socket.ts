8c6fe7ead48231cff6b21b66d72f534025826b88
Florian Guitton <florian@secretarium.com>
2023-10-06 13:31:45 +0100

    fix(connector): Change backing socket targeting

--- libs/connector/src/secretarium.socket.ts
+++ libs/connector/src/secretarium.socket.ts
@@ -1,8 +1,27 @@
-// import IsometricWS from '@d-fischer/isomorphic-ws';
 import * as NodeWebSocket from 'ws';
 
+declare global {
+    // eslint-disable-next-line no-var
+    var MozWebSocket: WebSocket;
+}
+
 const selectSocket = () => {
-    return typeof window !== 'undefined' ? window.WebSocket : NodeWebSocket;
+
+    let ws = undefined;
+
+    if (typeof WebSocket !== 'undefined') {
+        ws = WebSocket;
+    } else if (typeof MozWebSocket !== 'undefined') {
+        ws = MozWebSocket;
+    } else if (typeof global !== 'undefined') {
+        ws = global.WebSocket || global.MozWebSocket;
+    } else if (typeof window !== 'undefined') {
+        ws = window.WebSocket || window.MozWebSocket;
+    } else if (typeof self !== 'undefined') {
+        ws = self.WebSocket || self.MozWebSocket;
+    }
+
+    return typeof ws !== 'undefined' ? WebSocket : NodeWebSocket.WebSocket;
 };
 
 export default selectSocket();
